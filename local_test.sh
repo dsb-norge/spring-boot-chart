f0d2b395b5a5e0cea54a0f184851ab447bf127d0
Tommy Ballestad <tommy.ballestad@dsb.no>
2023-12-07 10:44:18 +0100

    Update test-application-values.yaml

--- local_test.sh
+++ local_test.sh
@@ -19,8 +19,10 @@ kind create cluster --config kind-cluster-config.yaml
 
 # Fetch test application used to test the charts:
 az acr login -n dsbacr
-docker pull dsbacr.azurecr.io/dsb-norge/test-application:2021.06.03.69273
-kind load docker-image dsbacr.azurecr.io/dsb-norge/test-application:2021.06.03.69273
+docker pull dsbacr.azurecr.io/dsb-norge/test-application:latest
+docker pull dsbacr.azurecr.io/dsb-norge/dsb-mssql-server:2019-latest
+kind load docker-image dsbacr.azurecr.io/dsb-norge/test-application:latest
+kind load docker-image dsbacr.azurecr.io/dsb-norge/dsb-mssql-server:2019-latest
 
 # Run Helm tests:
 docker run -it --rm --name ct --volume "$( pwd )":/data quay.io/helmpack/chart-testing:latest sh -c "ct install --all --debug --chart-dirs data/"
